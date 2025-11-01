3432b7b4a746816801a4f77446ec8e6fc2b99f1e
Avinash Malhotra <avimalhotra505@gmail.com>
2025-11-01 14:54:27 +0530

    useEffect

--- src/Example.jsx
+++ src/Example.jsx
@@ -2,68 +2,64 @@ import { useState, useEffect } from "react";
 
 export function Example() {
 
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [terms, setTerms] = useState(false);
-  const [gender, setGender] = useState("");
-  const [city, setCity] = useState("");
-
-  const [count, setCount]=useState(0);
+  const [counter,setCounter]=useState(0);
+  const [cars,setCars]=useState([]);
+  const [sec,setSec]=useState(0);
 
 
-  function sendData(e){
-    e.preventDefault();
-    console.log(e.target);
+  // console.log("app starts");
+
+  function changeCounter(){ 
+    console.log("change");
+    setCounter(counter=>counter+1);
   }
 
+  function addCars(e){
+    e.preventDefault();
+    setCars([...cars,e.target.car.value]);
+  }
 
-  // useEffect(()=>{ console.log("use effect") });
-  useEffect(()=>{ console.log("use effect") },[count]);
+  useEffect(()=>{
+    document.querySelector("form input").value="";
+    // console.log("app done");
+  });            
+  // useEffect(()=>{console.log("app done")},[counter]);            // with dependencies
 
-  console.log("app starts");
 
+  useEffect(()=>{
+        const id=setInterval(()=>{
+            setSec(sec=>sec+1);
+        },1000);
+        
+        return ()=>clearInterval(id);
+  },[]);
+
+  
   return (
     <>
       <h2>Form Example</h2>
 
-      <form onSubmit={sendData}>
-          <div>
-            <label>Name: <input className="input" type="text" name="name" value={name} onChange={(e)=>{setName(e.target.value)}} required /></label> <output>{name}</output>
-          </div>
-         <div>
-           <label>Age: <input className="input" type="number" name="age" value={age} onChange={(e)=>{setAge(e.target.valueAsNumber)}}  required/></label>
-            <output>{age}</output>
-         </div>
-         <div>
-           <label> <input type="checkbox" name="terms" value={terms} onChange={(e)=>{setTerms(e.target.checked)}} required/> : Terms and Conditions </label>
-           <output> {terms ? (<b>Agree</b>) : (<i>Disagree</i>) }</output>
-         </div>
-         <div>
-            <label> <input checked={gender === "male"} onChange={(e)=>{setGender(e.target.value)}} type="radio" name="gender" value="male" required /> Male</label>
-            <label> <input checked={gender === "female"} onChange={(e)=>{setGender(e.target.value)}} type="radio" name="gender" value="female" required /> Female</label>  
-            <output> {gender}</output>
-         </div>
-         <div>
-          <label>City: <select value={city} required name="city" onChange={(e)=>{setCity(e.target.value)}} className="input">
-            <option disabled selected value="">--Choose City--</option>
-            <option>Delhi</option>
-            <option>Mumbai</option>
-            <option>Kolkata</option>
-            <option>Chennai</option>
-            </select></label> 
-            <output>{city}</output>
-            </div>
-            <div>
-              <button className="btn" type="submit">Submit</button>
-            </div>
-      </form>
+      {/* <button className="btn" onClick={()=>setCounter(counter+1)}>Increment</button> */}
+      <button className="btn" onClick={changeCounter}>Increment</button>
+      <output>{counter}</output>
+      <hr />
+
+     <form onSubmit={addCars}>
+       <label>To do : <input className="input" type="text" name="car" required /> </label>
+      <button className="btn">Add</button>
+     </form>
+
+      <ol>
+      {
+        cars.map((elem,index)=>
+        <li key={index}>{elem}</li>
+        )
+      }
+      </ol>
 
       <hr />
-  <h2>Counter</h2>
-  <button className="btn" onClick={()=>setCount(count-1)}>Decrement</button>
-  <output>{count}</output>
-  <button className="btn" onClick={()=>setCount(count+1)}>Increment</button>
-  
+      <p>Sec: {sec}</p>
+
   </>
   );
 }
