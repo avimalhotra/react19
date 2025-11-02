e418cdc81c3eaa75ae589f5de48202837740a310
Avinash Malhotra <avimalhotra505@gmail.com>
2025-11-02 13:36:45 +0530

    useRef

--- src/Example.jsx
+++ src/Example.jsx
@@ -1,64 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function Example() {
 
-  const [counter,setCounter]=useState(0);
-  const [cars,setCars]=useState([]);
-  const [sec,setSec]=useState(0);
+    const ref=useRef(0);          // initial value;
+    const refInputName=useRef(null);
+    const refInputMail=useRef(null);
+
+    console.log(ref);
+    
+    // ref.current=1;
+    
+    // console.log(ref);
+
+    // console.log( refInputName );
 
 
-  // console.log("app starts");
+    function focusInput(){
+        // refInputName.current.focus();
+        refInputMail.current.focus();
+    }
 
-  function changeCounter(){ 
-    console.log("change");
-    setCounter(counter=>counter+1);
-  }
-
-  function addCars(e){
-    e.preventDefault();
-    setCars([...cars,e.target.car.value]);
-  }
-
-  useEffect(()=>{
-    document.querySelector("form input").value="";
-    // console.log("app done");
-  });            
-  // useEffect(()=>{console.log("app done")},[counter]);            // with dependencies
-
-
-  useEffect(()=>{
-        const id=setInterval(()=>{
-            setSec(sec=>sec+1);
-        },1000);
-        
-        return ()=>clearInterval(id);
-  },[]);
-
+    function changeRef(){
+      ref.current=ref.current+1;
+       console.log(ref);
+    }
   
   return (
     <>
       <h2>Form Example</h2>
 
-      {/* <button className="btn" onClick={()=>setCounter(counter+1)}>Increment</button> */}
-      <button className="btn" onClick={changeCounter}>Increment</button>
-      <output>{counter}</output>
-      <hr />
+      {/* <p>ref: {ref.current}</p> */}
 
-     <form onSubmit={addCars}>
-       <label>To do : <input className="input" type="text" name="car" required /> </label>
-      <button className="btn">Add</button>
-     </form>
+      <label>Name: <input type="text" ref={refInputName} /> </label>
+      <label>Email: <input type="text" ref={refInputMail} /> </label>
 
-      <ol>
-      {
-        cars.map((elem,index)=>
-        <li key={index}>{elem}</li>
-        )
-      }
-      </ol>
+      <button onClick={focusInput}>Click</button>
 
       <hr />
-      <p>Sec: {sec}</p>
+
+      <button onClick={changeRef}>Change</button> <span>{ref.current}</span>
 
   </>
   );
